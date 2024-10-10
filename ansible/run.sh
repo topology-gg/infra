5a10560c78d5ccddbe2e7b6edb4274f2604b0a97
droak <me+git@droak.sh>
2024-10-11 01:13:08 +0900

    add extra commands in run script; create docker setup playbook

--- ansible/run.sh
+++ ansible/run.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-ansible-playbook -i $SCRIPT_DIR/inventories/hosts.ini $SCRIPT_DIR/playbooks/$1.yaml
+ansible-playbook -i $SCRIPT_DIR/inventories/hosts.ini $SCRIPT_DIR/playbooks/$1.yaml ${@:2}
